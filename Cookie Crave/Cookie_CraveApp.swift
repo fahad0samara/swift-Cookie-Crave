02b06ed3234e710286c0bcbbfb6de74240ce7475
fahad0samarar <fah8ad@outlook.com>
2024-03-04 21:44:57 +0200

    add profileHeader

--- Cookie Crave/Cookie_CraveApp.swift
+++ Cookie Crave/Cookie_CraveApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Cookie_CraveApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ Cookie Crave/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  Cookie Crave
+//
+//  Created by fahad samara on 3/4/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ProfileHeaderView()
+        Spacer()
+    }
+}
+
+#Preview {
+    HomeView()
+}
