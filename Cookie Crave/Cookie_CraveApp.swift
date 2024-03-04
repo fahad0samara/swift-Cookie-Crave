934a2fbde2df2ebe910153b38f06e3dcf91d623f
fahad0samarar <fah8ad@outlook.com>
2024-03-04 10:53:36 +0200

    frist commit

---
+++ Cookie Crave/Cookie_CraveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_CraveApp.swift
+//  Cookie Crave
+//
+//  Created by fahad samara on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_CraveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
